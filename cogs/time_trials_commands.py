5c6a1a87dad182f1f316854b7b32a3475dee1c5d
Lucas Brooke <46628556+LJBrooke@users.noreply.github.com>
2026-06-03 22:04:36 +0200

    Type cast deez

--- cogs/time_trials_commands.py
+++ cogs/time_trials_commands.py
@@ -453,7 +453,7 @@ class TimeTrialsCommand(commands.Cog):
             target_vh,        # $3
             activity.value,   # $4
             tag,              # $5
-            level,            # $6
+            int(level),            # $6
             active_excluders  # $7
         )
         if not results:
